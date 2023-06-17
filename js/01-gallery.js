33c1880a00dae29ac8ba558732584ee6827a1bd1
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-06-18 00:53:50 +0300

    12

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    list : document.querySelector('.gallery'),
 
-console.log(galleryItems);
+};
+refs.list.addEventListener('click', onItemClick );
+
+
+function onItemClick(event){
+    if(!event.target.classList.contains('gallery__image')){
+        
+        return;
+        
+    }
+    console.log(event.target.dataset.source);
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+        <img src ="${event.target.dataset.source}" width="800" height="600">
+    `);
+    
+    
+    instance.show();
+
+}
+
+// створення розмітки
+const listItemsGalery = creatItemsGalery(galleryItems);
+console.log(listItemsGalery)
+function creatItemsGalery(items){
+   return items.map(item=>`<li class=".gallery__item"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"> </li>`).join(``);
+}
+
+refs.list.innerHTML = listItemsGalery;
+
+// делегація
+
