29518d3124085adccb4ff2d9277bb1efabb955c1
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-06-18 11:48:59 +0300

    sdg

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,7 +28,11 @@ function onItemClick(event){
 const listItemsGalery = creatItemsGalery(galleryItems);
 console.log(listItemsGalery)
 function creatItemsGalery(items){
-   return items.map(item=>`<li class=".gallery__item"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"> </li>`).join(``);
+   return items.map(item=>
+    `<li class=".gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}">
+    </a> </li>`).join(``);
 }
 
 refs.list.innerHTML = listItemsGalery;
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,4 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+    list : document.querySelector('.gallery'),
 
+};
+refs.list.addEventListener('click', onItemClick );
+
+
+function onItemClick(event){
+    if(!event.target.classList.contains('gallery__image')){
+    return;
+    }
+    var lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+}
+
+// створення розмітки
+const listItemsGalery = creatItemsGalery(galleryItems);
+console.log(listItemsGalery)
+function creatItemsGalery(items){
+   return items.map(item=>
+    `<li class=".gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+    </a> </li>`).join(``);
+}
+
+refs.list.innerHTML = listItemsGalery;
