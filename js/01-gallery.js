fe7fc8321f76e2697510489605e2001f9caa7773
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-06-18 14:37:55 +0300

    а

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -29,7 +29,7 @@ const listItemsGalery = creatItemsGalery(galleryItems);
 console.log(listItemsGalery)
 function creatItemsGalery(items){
    return items.map(item=>
-    `<li class=".gallery__item">
+    `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
     <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}">
     </a> </li>`).join(``);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,34 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
 const refs = {
     list : document.querySelector('.gallery'),
 
 };
-refs.list.addEventListener('click', onItemClick );
-
-
+const listItemsGalery = creatItemsGalery(galleryItems);
+refs.list.insertAdjacentHTML("beforeend", listItemsGalery);
+document.addEventListener('DOMContentLoaded', onItemClick);
 function onItemClick(event){
-    if(!event.target.classList.contains('gallery__image')){
-    return;
-    }
+    event.preventDefault();
     var lightbox = new SimpleLightbox('.gallery a', {
         captionsData: "alt",
         captionDelay: 250,
     });
 
-}
+};
 
 // створення розмітки
-const listItemsGalery = creatItemsGalery(galleryItems);
-console.log(listItemsGalery)
+
+
 function creatItemsGalery(items){
    return items.map(item=>
-    `<li class=".gallery__item">
+    `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
     <img class="gallery__image" src="${item.preview}" alt="${item.description}">
     </a> </li>`).join(``);
-}
+};
 
-refs.list.innerHTML = listItemsGalery;
+
